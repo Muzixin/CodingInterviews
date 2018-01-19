323e751771403b297f1945cec87d7456f6fec382
Muzi <islijie@foxmail.com>
2018-01-19 15:08:18 +0800

    复制复杂链表

--- src/questions/Q35.java
+++ src/questions/Q35.java
@@ -2,6 +2,10 @@ package questions;
 
 import util.RandomListNode;
 
+/**
+ * @author Azreal
+ *	复制复杂链表
+ */
 public class Q35 {
 
 	public RandomListNode Clone(RandomListNode pHead) {
@@ -9,7 +13,6 @@ public class Q35 {
 		CloneNodes(pHead);
 		ConnectRandomNodes(pHead);
 		return ReconnectNodes(pHead);
-
 	}
 
 	//复制原始列表的任意节点N，创建新的节点N'，并将N'加入到N的后面
@@ -17,7 +20,7 @@ public class Q35 {
 		
 		RandomListNode currentNode = pHead;
 		while(currentNode != null){
-			RandomListNode ClonedNode = null;
+			RandomListNode ClonedNode = new RandomListNode(0);
 			ClonedNode.label = currentNode.label;
 			ClonedNode.next = currentNode.next;
 			ClonedNode.random = null;
@@ -45,7 +48,7 @@ public class Q35 {
 		RandomListNode ClonedNode = null;
 		if(currentNode != null){
 			ClonedHead = ClonedNode = currentNode.next;
-			currentNode.next = currentNode.next;
+			currentNode.next = ClonedNode.next;
 			currentNode = currentNode.next;
 		}
 		
@@ -60,6 +63,37 @@ public class Q35 {
 	}
 	
 	public static void main(String[] args) {
-
+		RandomListNode node1 = new RandomListNode(1);
+		RandomListNode node2 = new RandomListNode(2);
+		RandomListNode node3 = new RandomListNode(3);
+		RandomListNode node4 = new RandomListNode(4);
+		RandomListNode node5 = new RandomListNode(5);
+		
+		node1.random = node3;
+		node2.random = node5;
+		node3.random = null;
+		node4.random = node2;
+		node5.random = null;
+		
+		node4.next = node5;
+		node3.next = node4;
+		node2.next = node3;
+		node1.next = node2;
+		
+		RandomListNode root = node1;
+		
+		Q35 q35 = new Q35();
+		RandomListNode listNode = q35.Clone(root);
+		
+		while(listNode != null){
+			System.out.print("Label: " + listNode.label + "  Random: ");
+			if(listNode.random != null)
+				System.out.print(listNode.random.label + "\n");
+			else {
+				System.out.print("# \n");
+			}
+			listNode = listNode.next;
+		}
+			
 	}
 }
